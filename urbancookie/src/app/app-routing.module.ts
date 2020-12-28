1649be0fed0b08b75419e3fee1535ee9fbff6d1f
Diana Coelho <coelhodiana04@gmail.com>
2020-12-28 12:29:23 -0300

    add routes

--- urbancookie/src/app/app-routing.module.ts
+++ urbancookie/src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { GaleriaComponent } from './galeria/galeria.component';
+import { HomeComponent } from './home/home.component';
+import { ShopComponent } from './shop/shop.component';
+import { SobreComponent } from './sobre/sobre.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  {path: 'home', component: HomeComponent},
+  {path: 'sobre', component: SobreComponent},
+  {path: 'galeria', component: GaleriaComponent},
+  {path: 'loja', component: ShopComponent},
+  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- urbancookie/src/app/app.module.ts
+++ urbancookie/src/app/app.module.ts
@@ -6,13 +6,19 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { FooterComponent } from './footer/footer.component';
+import { SobreComponent } from './sobre/sobre.component';
+import { ShopComponent } from './shop/shop.component';
+import { GaleriaComponent } from './galeria/galeria.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     NavbarComponent,
-    FooterComponent
+    FooterComponent,
+    SobreComponent,
+    ShopComponent,
+    GaleriaComponent
   ],
   imports: [
     BrowserModule,
